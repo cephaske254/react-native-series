f36bd6c238ec6021dea6a1e382a783baab5bd3cb
cephaske254 <cephaske254@gmail.com>
2023-12-30 15:15:55 +0300

    update:implement animations on indicators

--- pet-care-001/src/@models/onboarding.ts
+++ pet-care-001/src/@models/onboarding.ts
@@ -1,7 +1,24 @@
+import { SharedValue } from "react-native-reanimated";
+
 export type OnboardingItemType = {
   titles: string[];
   descriptions: string[];
 
   /** Image is a number since `require(...)` returns a number type */
   image: number;
+};
+
+export type CommonComponentProps = {
+  /**
+   * A value between 0 and 1, representing how far the user has scrolled
+   */
+  scrollProgress: SharedValue<number>;
+  /**
+   * How far the user has scrolled in pixels. This is calculated from `scrollProgress`
+   */
+  scrollOffset: SharedValue<number>;
+  /**
+   * The index of the item, calculated from the scroll progress.
+   */
+  scrollOffsetIndex: SharedValue<number>;
 };
